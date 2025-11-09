4ff7739affa42d6f16212b092501116ee54c07db
KimDaegyu <136952602+KimDaegyu-Dev@users.noreply.github.com>
2025-11-10 05:17:38 +0900

    style: 작품 추가 컴포넌트 추가(기능 미구현)

---
+++ src/components/AddSeries/AddSeries.tsx
@@ -0,0 +1,23 @@
+import { FC, ButtonHTMLAttributes } from "react";
+import { cn } from "@/utils";
+
+interface AddSeriesProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+}
+
+export const AddSeries: FC<AddSeriesProps> = ({ className, ...props }) => {
+  return (
+    <button
+      type="button"
+      className={cn("gap-xs flex h-262 w-150 flex-col items-center", className)}
+      {...props}
+    >
+      <div className="bg-background-subtle border-grayscale-g2 relative flex h-225 w-150 shrink-0 items-center justify-center rounded-sm border-2 border-dashed">
+        <span className="text-grayscale-g3 text-4xl">+</span>
+      </div>
+      <p className="text-body-medium w-min-content min-w-full text-center tracking-tight text-black">
+        연재 작품 추가하기
+      </p>
+    </button>
+  );
+};
